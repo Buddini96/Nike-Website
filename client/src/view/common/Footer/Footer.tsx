8164fee2d4fdd53ba4a63414cab5fc2352a0595a
buddini96 <buddinikl1996@gmail.com>
2023-12-24 20:00:39 +0530

    Home page added

--- client/src/view/common/Footer/Footer.tsx
+++ client/src/view/common/Footer/Footer.tsx
@@ -44,7 +44,7 @@ export const footerLinks = [
         title: "Get in touch",
         links: [
             { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
+            { name: "Buddini Pathianyaka", link: "tel:+768861459" },
         ],
     },
 ];
