0dc8bb27ab4e02007b4fa76840377626f88c08a5
buddini96 <buddinikl1996@gmail.com>
2023-12-24 11:05:32 +0530

    footer updated

--- client/src/view/common/Footer/Footer.tsx
+++ client/src/view/common/Footer/Footer.tsx
@@ -9,6 +9,8 @@ import  facebook from "../../../images/icons/facebook.svg"
 import  instagram from "../../../images/icons/instagram.svg"
 // @ts-ignore
 import  twitter from "../../../images/icons/twitter.svg"
+// @ts-ignore
+import copyrightSign from "../../../images/icons/copyright-sign.svg"
 
 export const socialMedia = [
     { src: facebook, alt: "facebook logo" },
@@ -16,34 +18,99 @@ export const socialMedia = [
     { src: instagram, alt: "instagram logo" },
 ];
 
+export const footerLinks = [
+    {
+        title: "Products",
+        links: [
+            { name: "Air Force 1", link: "/" },
+            { name: "Air Max 1", link: "/" },
+            { name: "Air Jordan 1", link: "/" },
+            { name: "Air Force 2", link: "/" },
+            { name: "Nike Waffle Racer", link: "/" },
+            { name: "Nike Cortez", link: "/" },
+        ],
+    },
+    {
+        title: "Help",
+        links: [
+            { name: "About us", link: "/" },
+            { name: "FAQs", link: "/" },
+            { name: "How it works", link: "/" },
+            { name: "Privacy policy", link: "/" },
+            { name: "Payment policy", link: "/" },
+        ],
+    },
+    {
+        title: "Get in touch",
+        links: [
+            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+            { name: "+92554862354", link: "tel:+92554862354" },
+        ],
+    },
+];
+
 export const Footer = () => {
     return (
-        <footer className='max-container'>
-            <div className={"flex justify-between items-start gap-20 flex-wrap max-lg:flex-col bg-black"}>
-                <div className='flex flex-col items-start'>
-                {/*<p className="text[10px] text-[#e6f0e6]*/}
-                {/*                  pl-2 hover:text-green-400 pt-1.5">*/}
-                {/*    Copyright @ 2023</p>*/}
-
-                {/*<h1 className={"text-2xl text-[#F3F8FF] pl-2 "}>Organic Shop</h1>*/}
-                <img className={"m-0 w-[150px] h-[46px]"} src={footerLogo} alt={''}></img>
+        <footer className='max-container bg-black'>
+            <div className='flex justify-between items-start gap-2 flex-wrap pt-16'>
+                <div className='flex flex-col items-start pl-12'>
+                    <a href='/'>
+                        <img
+                            src={footerLogo}
+                            alt='logo'
+                            width={150}
+                            height={46}
+                            className='m-0'
+                        />
+                    </a>
                     <p className='mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm'>
                         Get shoes ready for the new term at your nearest Nike store. Find
                         Your perfect Size In Store. Get Rewards
                     </p>
-                    <div className={"flex items-center gap-5 mt-8"}>
+
+                    <div className='flex items-center gap-5 mt-8'>
                         {socialMedia.map((icon) => (
                             <div
                                 className='flex justify-center items-center w-12 h-12 bg-white rounded-full'
                                 key={icon.alt}
                             >
-                        {/*<img className={"m-0 w-[150px] h-[46px]"} src={facebook} alt={'facebook logo'}></img>*/}
-                        {/*/!*<img className={"m-0 w-[150px] h-[46px]"} src={instagram} alt={'instagram logo'}></img>*!/*/}
-                        {/*<img className={"m-0 w-[150px] h-[46px]"} src={twitter} alt={'twitter logo'}></img>*/}
                                 <img src={icon.src} alt={icon.alt} width={24} height={24} />
                             </div>
-                            ))}
+                        ))}
                     </div>
+                </div>
+
+                <div className='flex flex-1 justify-between pr-12 flex-wrap pl-10'>
+                    {footerLinks.map((section) => (
+                        <div key={section.title}>
+                            <h4 className='font-montserrat text-2xl leading-normal font-medium mb-6 text-white pr-12 pl-12'>
+                                {section.title}
+                            </h4>
+                            <ul>
+                                {section.links.map((link) => (
+                                    <li
+                                        className='mt-3 font-montserrat text-base leading-normal text-white-400 hover:text-slate-gray pr-12 pl-12'
+                                        key={link.name}
+                                    >
+                                        <a href={link.link}>{link.name}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className='flex justify-center text-white-400 mt-24 max-sm:flex-col max-sm:items-center pr-5 '>
+                <div className='flex flex-1 justify-center items-center gap-2 font-montserrat cursor-pointer'>
+                    <img
+                        src={copyrightSign}
+                        alt='copyright sign'
+                        width={20}
+                        height={20}
+                        className='rounded-full m-0'
+                    />
+                    <p>Copyright. All rights reserved.</p>
                 </div>
             </div>
         </footer>
