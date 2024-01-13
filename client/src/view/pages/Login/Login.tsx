488afa324aa169b60c2dac9f5623bc7ba7e94a5a
buddini96 <buddinikl1996@gmail.com>
2024-01-13 23:26:15 +0530

    content updated

--- client/src/view/pages/Login/Login.tsx
+++ client/src/view/pages/Login/Login.tsx
@@ -101,62 +101,62 @@ export class Login extends Component {
                 <div className={"w-1/2 h-full bg-[#F5F5F5] flex flex-col p-20 justify-between items-center"}>
 
                     {/*<h1 className={"w-full max-w-[500px] mx-auto text-xl text-[#060606] font-semibold mr-auto"}>Nike</h1>*/}
-                <img className={"w-[90px] h-[70px] pb-4"} src={nikeLogo}/>
+                    <img className={"w-[90px] h-[70px] pb-4"} src={nikeLogo}/>
                     {/*mr-[27rem]*/}
 
-                <form onSubmit={this.handleSubmit}>
-                <div className={"w-full flex flex-col  max-w-[500px]"}>
-                    <div className={"w-full flex flex-col mb-2"}>
-                    <h3 className={"text-3xl font-semibold mb-2"}>Login</h3>
-                    <p className={"text-base mb-2"}>Welcome Back! Please enter your details.</p>
-                    </div>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className={"w-full flex flex-col  max-w-[500px]"}>
+                            <div className={"w-full flex flex-col mb-2"}>
+                                <h3 className={"text-3xl font-semibold mb-2"}>Login</h3>
+                                <p className={"text-base mb-2"}>Welcome Back! Please enter your details.</p>
+                            </div>
 
-                    <div className={"w-full flex flex-col"}>
-                        <input
-                            type={"email"}
-                            placeholder={"Email"}
-                            onChange={(e) => this.setState({email: e.target.value})}
-                            className={"w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none"}/>
+                            <div className={"w-full flex flex-col"}>
+                                <input
+                                    type={"email"}
+                                    placeholder={"Email"}
+                                    onChange={(e) => this.setState({email: e.target.value})}
+                                    className={"w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none"}/>
 
-                        <input
-                            type={"password"}
-                            placeholder={"Password"}
-                            onChange={(e) => this.setState({password: e.target.value})}
-                            className={"w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none"}/>
-                    </div>
+                                <input
+                                    type={"password"}
+                                    placeholder={"Password"}
+                                    onChange={(e) => this.setState({password: e.target.value})}
+                                    className={"w-full text-black py-2 my-2 bg-transparent border-b border-black outline-none focus:outline-none"}/>
+                            </div>
 
-                    <div className={"w-full flex items-center justify-center"}>
-                        <div className={"w-full flex items-center"}>
-                           <input type={"checkbox"} className={"w-4 h-4 mr-2"}/>
-                            <p className={"text-sm"}>Remember Me for 30 days</p>
+                            <div className={"w-full flex items-center justify-center"}>
+                                <div className={"w-full flex items-center"}>
+                                    <input type={"checkbox"} className={"w-4 h-4 mr-2"}/>
+                                    <p className={"text-sm"}>Remember Me for 30 days</p>
+                                </div>
+
+                                <p className={"text-sm font-medium whitespace-nowrap cursor-pointer underline underline-offset-2"}>Forgot Password ?</p>
+                            </div>
+
+                            <div className={"w-full flex flex-col my-4"}>
+                                <button type={"submit"} className={"w-full my-2 font-semibold text-white bg-[#060606] rounded-md p-4 text-center flex items-center justify-center cursor-pointer"}>
+                                    <Link to={'/userDashboard'}>
+                                        Log in
+                                    </Link>
+                                </button>
+                                <button className={"w-full my-2 font-semibold text-[#060606] bg-white border  border-black rounded-md p-4 text-center flex items-center justify-center cursor-pointer"}>
+                                    <Link to={'/Register'}>
+                                        Register
+                                    </Link>
+                                </button>
+                            </div>
+
+                            <div className={"w-full flex items-center justify-center relative py-2"}>
+                                <div className={"w-full h-[1px] bg-black/40"}></div>
+                                <p className={"text-lg absolute text-black/80 bg-[#F5F5F5]"}>or</p>
+                            </div>
+                            <div className={"w-full my-2 font-semibold text-[#060606] bg-white border  border-black/40 rounded-md p-4 text-center flex items-center justify-center cursor-pointer"}>
+                                <img src={google} className={"h-6 mr-2"}/>
+                                Sign In with Google
+                            </div>
                         </div>
-
-                        <p className={"text-sm font-medium whitespace-nowrap cursor-pointer underline underline-offset-2"}>Forgot Password ?</p>
-                    </div>
-
-                    <div className={"w-full flex flex-col my-4"}>
-                        <button type={"submit"} className={"w-full my-2 font-semibold text-white bg-[#060606] rounded-md p-4 text-center flex items-center justify-center cursor-pointer"}>
-                            <Link to={'/userDashboard'}>
-                                Log in
-                            </Link>
-                        </button>
-                        <button className={"w-full my-2 font-semibold text-[#060606] bg-white border  border-black rounded-md p-4 text-center flex items-center justify-center cursor-pointer"}>
-                            <Link to={'/Register'}>
-                            Register
-                            </Link>
-                        </button>
-                    </div>
-
-                    <div className={"w-full flex items-center justify-center relative py-2"}>
-                        <div className={"w-full h-[1px] bg-black/40"}></div>
-                        <p className={"text-lg absolute text-black/80 bg-[#F5F5F5]"}>or</p>
-                    </div>
-                    <div className={"w-full my-2 font-semibold text-[#060606] bg-white border  border-black/40 rounded-md p-4 text-center flex items-center justify-center cursor-pointer"}>
-                        <img src={google} className={"h-6 mr-2"}/>
-                        Sign In with Google
-                    </div>
-                </div>
-                 </form>
+                    </form>
 
                     <div className={"w-full flex items-center justify-center "}>
                         <p className={"text-sm font-normal text-[#060606]"}>Don't have a account? <span className={"font-semibold underline underline-offset-2"}>Sign up for free</span></p>
